4db1aeffe08e37518c78da6785e294411eda2c8c
Allen Thomas Varghese <allentv4u@gmail.com>
2020-12-05 13:55:15 +0000

    Replace actix with rocket

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
-// This is taken from the offical hello world example for actix
-use actix_web::{middleware, web, App, HttpServer};
+#![feature(proc_macro_hygiene, decl_macro)]
 
-mod handlers;
+use rocket::*;
 
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=debug");
-    env_logger::init();
+#[get("/<code>")]
+fn code(code: String) -> String {
+    return format!("Received code: {}", code);
+}
 
-    HttpServer::new(|| {
-        App::new()
-            // enable logger
-            .wrap(middleware::Logger::default())
-            .route("/index.html", web::get().to(handlers::index))
-            .route("/", web::get().to(handlers::index))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+fn main() {
+    rocket::ignite().mount("/", routes![code]).launch();
 }
