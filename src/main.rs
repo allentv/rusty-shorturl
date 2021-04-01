272ffc572fd5331a3d56b2e8f60381d6d18b04b4
Allen Thomas Varghese <allentv4u@gmail.com>
2021-04-01 22:49:11 +0100

    Combine everything in one file

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+use rocket_contrib::databases::diesel;
 
-mod handlers;
+#[database("shorty_db")]
+pub struct ShortyDBConn(diesel::SqliteConnection);
+
+use rocket::response::NamedFile;
+use std::path::{Path, PathBuf};
+
+#[get("/<code>")]
+fn code(_conn: ShortyDBConn, code: String) -> String {
+    return format!("Received code: {}", code);
+}
+
+#[get("/ping")]
+fn ping() -> String {
+    return String::from("pong");
+}
+
+#[get("/static/<file..>")]
+fn static_files(file: PathBuf) -> Option<NamedFile> {
+    return NamedFile::open(Path::new("static/").join(file)).ok();
+}
 
 fn main() {
-    let mut mnt = rocket::ignite();
+    let mut mnt = rocket::ignite().attach(ShortyDBConn::fairing());
 
     // Add root routes
-    mnt = mnt.mount(
-        "/",
-        routes![handlers::code, handlers::static_files, handlers::ping,],
-    );
+    mnt = mnt.mount("/", routes![code, static_files, ping,]);
 
     // Start the web server
     mnt.launch();
